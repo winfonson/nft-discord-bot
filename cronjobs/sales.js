beb92ba2261ddd50daedaba27c38f95989ce0406
winfonson <41123529+winfonson@users.noreply.github.com>
2021-09-19 11:44:19 +0800

    mint

--- cronjobs/sales.js
+++ cronjobs/sales.js
@@ -78,3 +78,77 @@ module.exports = {
     lastTimestamp = newTimestamp;
   }
 };
+
+module.exports = {
+  name: 'mint',
+  description: 'mint bot!',
+  interval: 30000,
+  async execute(client) {
+    if (lastTimestamp == null) {
+      lastTimestamp = Math.floor(Date.now()/1000) - 120;
+    } else {
+      lastTimestamp -= 30;
+    }
+    let newTimestamp = Math.floor(Date.now()/1000) - 30;
+    // we're retrieving events from -90 to -30 seconds ago each time, and each query overlaps the previous query by 30 seconds
+    // doing this to try to resolve some intermittent issues with events being missed by the bot, suspect it's due to OpenSea api being slow to update the events data
+    // duplicate events are filtered out by the salesCache array
+
+    let offset = 0;
+    let settings = { 
+      method: "GET",
+      headers: {
+        "X-API-KEY": process.env.OPEN_SEA_API_KEY
+      }
+    };
+    while(1)
+    {
+
+      let url = `${openseaEventsUrl}?collection_slug=${process.env.OPEN_SEA_COLLECTION_NAME}&event_type=created&only_opensea=false&offset=${offset}&limit=50&occurred_after=${lastTimestamp}&occurred_before=${newTimestamp}`;
+      try {
+        var res = await fetch(url, settings);
+        if (res.status != 200) {
+          throw new Error(`Couldn't retrieve events: ${res.statusText}`);
+        }
+
+        data = await res.json();
+        if (data.asset_events.length == 0) {
+          break;
+        }
+
+        data.asset_events.forEach(function(event) {
+          if (event.asset) {
+            if (salesCache.includes(event.id)) {
+              return;
+            } else {
+              salesCache.push(event.id);
+              if (salesCache.length > 200) salesCache.shift();
+            }
+
+            const embedMsg = new Discord.MessageEmbed()
+              .setColor('#0099ff')
+              .setTitle(event.asset.name)
+              .setURL(event.asset.permalink)
+              .setDescription(`has just been created!`)
+              .setThumbnail(event.asset.image_url)
+              .addField("From", `[${event.seller.user?.username || event.seller.address.slice(0,8)}](https://etherscan.io/address/${event.seller.address})`, true)
+              .addField("To", `[${event.winner_account.user?.username || event.winner_account.address.slice(0,8)}](https://etherscan.io/address/${event.winner_account.address})`, true);
+
+            client.channels.fetch(process.env.DISCORD_SALES_CHANNEL_ID)
+              .then(channel => {
+                channel.send(embedMsg);
+              })
+              .catch(console.error);
+          }
+        });
+      }
+      catch (error) {
+        console.error
+      }
+
+      offset += data.asset_events.length;
+    }
+
+    lastTimestamp = newTimestamp;
+  }
+};
